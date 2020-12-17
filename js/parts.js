be20bf440c4b453ff6058eb3c1e6ff9a61a620ee
松本　龍太郎 <71996880+ryutaro1234@users.noreply.github.com>
2020-12-18 02:20:01 +0900

    Update parts.js

--- js/parts.js
+++ js/parts.js
@@ -3,4 +3,12 @@ $(function(){
 	$("#header").load("parts/header.html");
 	$("#nav").load("parts/nav.html");
 	$("#footer").load("parts/footer.html");
-})
+});
+
+$(function(){
+    $('header').hover(function(){
+        $("nav:not(:animated)", this).slideDown();
+    }, function(){
+        $("nav",this).slideUp();
+    });
+});
