b7f078ebb758072230f1062f2b5dbe9ac98211e6
松本　龍太郎 <71996880+ryutaro1234@users.noreply.github.com>
2020-12-18 04:46:21 +0900

    Update parts.js

--- js/parts.js
+++ js/parts.js
@@ -4,13 +4,3 @@ $(function(){
 	$("#nav").load("parts/nav.html");
 	$("#footer").load("parts/footer.html");
 });
-
-$(function(){
-    $("nav").hide();
-    $("header").hover(function(){
-            $("nav:not(:animated)",this).slideDown("fast")
-        },
-        function(){
-            $("nav",this).slideUp("fast");
-    })
-}) 
