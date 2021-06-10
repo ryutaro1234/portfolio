53972fa1c86f1f57f8732c6fd5a86707f7cc12ce
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-06-10 22:45:48 +0900

    Delete js directory

--- js/parts.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-$(function(){
-	$("#header").load("https://ryutaro1234.github.io/portfolio/parts/header.html");
-	$("#nav").load("https://ryutaro1234.github.io/portfolio/parts/nav.html");
-	$("#footer").load("https://ryutaro1234.github.io/portfolio/parts/footer.html");
-});
