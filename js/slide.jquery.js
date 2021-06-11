a1148c16153828857196d6539a6a4749ad33a190
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-06-11 12:36:39 +0900

    Add files via upload

--- js/slide.jquery.js
+++ js/slide.jquery.js
@@ -1,14 +1,13 @@
 
 //クリックすると表示されるボタン
 $(function(){
-    $(".slide").click(function(){
-        var $this = $(this);
-        var $target = $this.next();
-        if($target.css('display') == 'none')
-{
-            $target.slideDown(400);
-        }else{
-            $target.slideUp(400);
-        }
-    });
+	$(".slide").click(function(){
+		var $this = $(this);
+		var $target = $this.next();
+		if($target.css('display') == 'none'){
+			$target.slideDown(400);
+		}else{
+			$target.slideUp(400);
+		}
+	});
 });
