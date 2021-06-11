dfc66b459d07c3e362df08a44f192bcc1d81a4a7
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-06-11 12:34:39 +0900

    Update slide.jquery.js

--- js/slide.jquery.js
+++ js/slide.jquery.js
@@ -4,12 +4,11 @@ $(function(){
     $(".slide").click(function(){
         var $this = $(this);
         var $target = $this.next();
-        if($target.css('display') == 'none'){
+        if($target.css('display') == 'none')
+{
             $target.slideDown(400);
-            $this.text("隠す");
         }else{
             $target.slideUp(400);
-            $this.text ($(".slide + li"))
         }
     });
 });
